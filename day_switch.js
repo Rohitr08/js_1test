1f8fbd575d8d62b72c59fbb8a42977698e9f7d95
Rohit kharwar <75599391+Rohitr08@users.noreply.github.com>
2022-05-29 21:37:24 +0530

    Add files via upload

---
+++ day_switch.js
@@ -0,0 +1,55 @@
+// switch day according to num of day
+let num = 1;
+switch (num) {
+  case 1:
+    day = "Sunday";
+    console.log(day);
+    break;
+  case 2:
+    day = "Monday";
+    console.log(day);
+    break;
+  case 3:
+    day = "Tuesday";
+    console.log(day);
+    break;
+  case 4:
+    day = "Wednesday";
+    console.log(day);
+    break;
+  case 5:
+    day = "Thursday";
+    console.log(day);
+    break;
+  case 6:
+    day = "Friday";
+    console.log(day);
+    break;
+  case 7:
+    day = "Saturday";
+    console.log(day);
+}
+
+
+//Check if a number is odd or even using if-else statement.
+let len = 10;
+let brd = 20;
+if (len == brd) {
+  console.log("square");
+
+} else {
+  console.log("rectangle");
+
+}
+
+//Check if a number is odd or even using if-else statement.
+let number =8;
+
+if (number%2==0) 
+{
+  console.log("even");
+}
+ else 
+ {
+  console.log("odd");
+}
